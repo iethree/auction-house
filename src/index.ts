5b445ca33deaf9e2a2702537dce903b7fd54cb9c
Ryan Laurie <iethree@gmail.com>
2022-10-26 16:12:17 -0600

    add time logging

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import loadAllItems from './loadItems';
 // fetch
 const skywallId = 86;
 
+console.time('🕒 Auction ETL job');
+console.time('🕒 Auction Fetch');
+
+
 console.log('⌛ fetching auctions', new Date());
 
 const auctionData = await getAuctionData(skywallId).catch(console.error);
@@ -24,13 +28,17 @@ const parsedPrices = consolidatePrices(auctionData);
 const insertedPrices = await saveAuctionData(parsedPrices).catch(console.error);
 
 console.log(`✅ ${insertedPrices} prices saved`);
-
 await loadAllItems();
+console.timeEnd('🕒 Auction Fetch');
+
+console.time('🕒 Craft Price Calculation');
 
 console.log('⌛ calculating craft prices');
 const loaded = await loadCraftPrices().catch(console.error);
 console.log(`✅ saved ${loaded} craft prices`);
-
 console.log(`✅ done`);
+
+console.timeEnd('🕒 Craft Price Calculation');
+console.timeEnd('🕒 Auction ETL job');
 
 process.exit(0);
